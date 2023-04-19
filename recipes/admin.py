4ef0bdd7f5931a6aa15c4fbf1f02553bf239a530
devaditya12345 <63456777+devaditya12345@users.noreply.github.com>
2023-04-19 17:34:48 +0530

    Writing admin.py of recipes app

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.contrib import admin
 
 # Register your models here.
+from .models import RecipeIngredient, Recipe
+
+User = get_user_model()
+
+admin.site.register(RecipeIngredient) #Seekhne ke liye acha hai
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    # fields = ['name', 'quanity', 'unit', 'directions']
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+admin.site.register(Recipe, RecipeAdmin)
